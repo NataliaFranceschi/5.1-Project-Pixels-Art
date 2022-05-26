7ad6afef84ac3d278009568b9f78fbb0067fdf47
Natalia Franceschi <natalia.gfranceschi@gmail.com>
2022-05-26 17:23:37 -0300

    Melhoris Lint

--- script.js
+++ script.js
@@ -1,25 +1,25 @@
+function corAleatoria() {
+  const red = parseInt(Math.random() * 255, 10);
+  const green = parseInt(Math.random() * 255, 10);
+  const blue = parseInt(Math.random() * 255, 10);
+  return `rgb(${red}, ${green}, ${blue})`;
+}
+
 document.getElementById('primeiraCor').style.background = 'black';
 document.getElementById('segundaCor').style.background = corAleatoria();
 document.getElementById('terceiraCor').style.background = corAleatoria();
 document.getElementById('quartaCor').style.background = corAleatoria();
 
-function corAleatoria() {
-  const red = parseInt(Math.random() * 255 , 10);
-  const green = parseInt(Math.random() * 255 , 10);
-  const blue = parseInt(Math.random() * 255 , 10);
-  return `rgb(${red}, ${green}, ${blue})`;
-}
-
 const divBoard = document.getElementById('pixel-board');
 
 function createBoard(tamanho) {
-    for (let i = 0; i < tamanho; i += 1) {
-      const divline = document.createElement('div');
-      divline.className = 'line';
-      divBoard.appendChild(divline);
-    }
-    const linha = document.getElementsByClassName('line');
-    for (let i = 0; i < linha.length; i += 1) {
+  for (let i = 0; i < tamanho; i += 1) {
+    const divline = document.createElement('div');
+    divline.className = 'line';
+    divBoard.appendChild(divline);
+  }
+  const linha = document.getElementsByClassName('line');
+  for (let i = 0; i < linha.length; i += 1) {
     for (let index = 0; index < linha.length; index += 1) {
       const pixel = document.createElement('div');
       pixel.className = 'pixel';
@@ -45,45 +45,48 @@ document.getElementById('quartaCor').addEventListener('click', trocaClasse);
 
 const pixel = document.getElementsByClassName('pixel');
 
-for (let index = 0; index < pixel.length; index += 1) {
-pixel[index].addEventListener('click', mudancaCor);
-}
-
 function mudancaCor(event) {
   const selected = document.getElementsByClassName('selected')[0];
   event.target.style.backgroundColor = selected.style.backgroundColor;
 }
 
-const buttonLimpar = document.getElementById('clear-board');
-buttonLimpar.addEventListener('click', function(){
-  const pixel = document.getElementsByClassName('pixel');
-  for (let i = 0; i < pixel.length; i += 1) {
-    pixel[i].style.backgroundColor = 'white'
+for (let index = 0; index < pixel.length; index += 1) {
+  pixel[index].addEventListener('click', mudancaCor);
+}
+
+function limpar() {
+  const pixels = document.getElementsByClassName('pixel');
+  for (let i = 0; i < pixels.length; i += 1) {
+    pixels[i].style.backgroundColor = 'white';
   }
-});
+}
+
+const buttonLimpar = document.getElementById('clear-board');
+buttonLimpar.addEventListener('click', limpar);
 
 const buttonCriar = document.getElementById('generate-board');
 const input = document.getElementsByTagName('input')[0];
 
-buttonCriar.addEventListener('click', criar);
-function criar() {
-    
-    if (input.value === '') {
-        return alert('Board inválido!')
-    } else if (input.value > 0 && input.value < 5 ) {
-      const line = document.getElementsByClassName('line');
-      for (let i = 0; i < 5; i += 1) {
-      line[0].remove()}
-      createBoard(5); 
-    } else if (input.value > 50) {
-      const line = document.getElementsByClassName('line');
-      for (let i = 0; i < 5; i += 1) {
-      line[0].remove()}
-      createBoard(50);
-    } else {
-      const line = document.getElementsByClassName('line');
-      for (let i = 0; i < 5; i += 1) {
-      line[0].remove()}
-      createBoard(input.value)
-    }
+function apagarBoard() {
+  const line = document.getElementsByClassName('line');
+  for (let i = 0; i < 5; i += 1) {
+    line[0].remove();
+  }
 }
+
+function criar() {
+  if (input.value === '') {
+    return alert('Board inválido!');
+  } if (input.value > 0 && input.value < 5) {
+    apagarBoard();
+    createBoard(5);
+  } else if (input.value > 50) {
+    apagarBoard();
+    createBoard(50);
+  } else {
+    apagarBoard();
+    createBoard(input.value);
+  }
+}
+
+buttonCriar.addEventListener('click', criar);
